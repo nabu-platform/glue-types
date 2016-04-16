e3bdc59ddb72fe5294078ee804daf1e7d917d112
alex <alex@nabu.be>
2016-04-16 08:16:16 +0200

    Allow masking of dynamic types to static

--- src/main/java/be/nabu/glue/types/ComplexTypeProvider.java
+++ src/main/java/be/nabu/glue/types/ComplexTypeProvider.java
@@ -1,16 +1,22 @@
 package be.nabu.glue.types;
 
+import java.util.Map;
+
+import be.nabu.glue.api.ExecutionContext;
 import be.nabu.glue.api.OptionalTypeConverter;
 import be.nabu.glue.api.OptionalTypeProvider;
 import be.nabu.libs.types.BaseTypeInstance;
+import be.nabu.libs.types.ComplexContentWrapperFactory;
 import be.nabu.libs.types.DefinedTypeResolverFactory;
 import be.nabu.libs.types.SimpleTypeWrapperFactory;
 import be.nabu.libs.types.TypeConverterFactory;
 import be.nabu.libs.types.api.ComplexContent;
+import be.nabu.libs.types.api.ComplexType;
+import be.nabu.libs.types.api.DefinedSimpleType;
 import be.nabu.libs.types.api.DefinedType;
-import be.nabu.libs.types.api.SimpleType;
 import be.nabu.libs.types.api.Type;
-import be.nabu.libs.types.java.BeanResolver;
+import be.nabu.libs.types.map.MapContent;
+import be.nabu.libs.types.mask.MaskedContent;
 
 public class ComplexTypeProvider implements OptionalTypeProvider {
 
@@ -28,25 +34,46 @@ public class ComplexTypeProvider implements OptionalTypeProvider {
 			this.type = type;
 		}
 		
+		@SuppressWarnings({ "unchecked", "rawtypes" })
 		@Override
 		public Object convert(Object object) {
 			if (object == null) {
 				return null;
 			}
+			// if it is a dynamic type, just mask it
+			else if (object instanceof ExecutionContext) {
+				return new MaskedContent(new MapContent((ComplexType) type, ((ExecutionContext) object).getPipeline()), (ComplexType) type);
+			}
+			else if (object instanceof Map) {
+				return new MaskedContent(new MapContent((ComplexType) type, (Map) object), (ComplexType) type);
+			}
+			
 			Type sourceType = null;
 			if (object instanceof ComplexContent) {
 				sourceType = ((ComplexContent) object).getType();
 			}
-			else if (type instanceof SimpleType) {
-				sourceType = SimpleTypeWrapperFactory.getInstance().getWrapper().wrap(type.getClass());
-			}
 			else {
-				sourceType = BeanResolver.getInstance().resolve(object.getClass());
+				DefinedSimpleType<? extends Object> wrap = SimpleTypeWrapperFactory.getInstance().getWrapper().wrap(object.getClass());
+				if (wrap != null) {
+					sourceType = wrap;
+				}
+				else {
+					object = ComplexContentWrapperFactory.getInstance().getWrapper().wrap(object);
+					if (object == null) {
+						throw new ClassCastException("Can not find type for object");
+					}
+					sourceType = ((ComplexContent) object).getType();
+				}
 			}
 			if (sourceType == null) {
 				throw new ClassCastException("Can not resolve the class " + type.getClass() + " to a type");
 			}
-			return TypeConverterFactory.getInstance().getConverter().convert(object, new BaseTypeInstance(sourceType), new BaseTypeInstance(type));
+			// check if converted is null? if it is null but source is not, we failed conversion?
+			Object convert = TypeConverterFactory.getInstance().getConverter().convert(object, new BaseTypeInstance(sourceType), new BaseTypeInstance(type));
+			if (convert == null && object != null) {
+				throw new ClassCastException("Can not cast " + object + " to type: " + type);
+			}
+			return convert;
 		}
 
 		@Override
